039bb91a64a7c65178dba99cf8be3d501bcf3574
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-07-08 23:51:49 +0530

    update

--- frontend/src/Component/Home.jsx
+++ frontend/src/Component/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         
         <div className="text-white mt-[100px] flex justify-center items-center flex-col">
             <h1 className="text-[45px] font-medium text-center">Welcome to CodeCamp!</h1>
-            <p className="text-[20px] mt-[10px] text-center px-4">Unleash your coding potential and embark on a journey to innovate, achieve, master, challenge, and succeed!</p>
+            <p className="text-[20px] mt-[10px] text-center px-4 text-green-700">Unleash your coding potential and embark on a journey to innovate, achieve, master, challenge, and succeed!</p>
         </div>
 
         <Card customClass = "mt-[60px] text-white flex justify-center gap-2 flex-wrap"/>
